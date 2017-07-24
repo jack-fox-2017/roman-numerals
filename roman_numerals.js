407a2a2f3ccfb6db33ad099a9d5c58d04aa8eb2e
jainal arifin <jainalarifin@jainals-MacBook-Pro.local>
2017-07-24 16:09:07 +0700

    roman_numerals jainal arifin

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,17 @@
 function to_roman (num) {
-  // your implementation code here
+  let decimal = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let romawi  = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+  let tampung = '';
+
+  for( let nilai = 12; nilai >=0; nilai--){
+    while( num >= decimal[nilai]){
+      num -= decimal[nilai]
+      tampung += romawi[nilai]
+    }
+  }
+
+
+  return tampung;
 }
 
 // Drive code
