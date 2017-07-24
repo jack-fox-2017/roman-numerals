9dc578b99dd09742356e21f73cb161006b823a9d
ahmadaidil <ahmdaidil@gmail.com>
2017-07-24 16:31:47 +0700

    roman-numerals selesai

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,15 @@
 function to_roman (num) {
   // your implementation code here
+  let roman = '';
+  let des = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let romanValue = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  for (let i = 0; i < des.length; i++) {
+    while (num >= des[i]) {
+      roman += romanValue[i];
+      num -= des[i];
+    }
+  }
+  return roman;
 }
 
 // Drive code
