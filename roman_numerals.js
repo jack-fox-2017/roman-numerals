09aa99455534a690a83b095191292498c4d20114
alfandash <cumiasem91@gmail.com>
2017-07-24 16:05:57 +0700

    Release 0: Roman Numerals Done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,20 @@
 function to_roman (num) {
   // your implementation code here
+  var digit = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  var roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+  var temp = [];
+  while(num>0){
+    for (var i = 0; i < digit.length; i++) {
+      if(digit[i]<=num) {
+          num = num - digit[i];
+          temp.push(roman[i]);
+      }
+      //console.log('num =',num);
+      //console.log('i =',i);
+    }
+      //console.log('temp = ',temp);
+  }
+  return  temp.join('');
 }
 
 // Drive code
