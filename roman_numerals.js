c6cdba9879da0f63e159a1220161bb54f0b6e20b
rizqjoe <rjoelisman@gmail.com>
2017-07-24 19:47:15 +0700

    tugas roman_numerals rizkqjoe

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,16 @@
 function to_roman (num) {
   // your implementation code here
+  var result = ''
+  var roman = ["MDCXLVI","MCDLIII","M","D","C","L","XIII","X","IX","V","IV","I"];
+  var latin = [1646,1453,1000,500,100,50,13,10,9,5,4,1];
+
+  for (var i = 0; i < latin.length; i++) {
+    while (num >= latin [i]) {
+      result += roman[i];
+      num = num - latin[i];
+    }
+  }
+    return result;
 }
 
 // Drive code
