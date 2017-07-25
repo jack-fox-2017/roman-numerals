d0dd072338273b9191f16bf7ecb7b60fad0f8611
dennymiloen <dennymiloen@gmail.com>
2017-07-24 19:27:59 -0700

    tugas roman_numerals1

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,22 @@
 function to_roman (num) {
   // your implementation code here
+let roman_arr = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+let arabic_arr = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+let tamp = "";
+
+	for (let i = arabic_arr.length; i >= 0; i--) {
+		if (num === arabic_arr[i]) {
+			tamp += roman_arr[i];
+			return tamp;
+		} 
+		else if (num >= arabic_arr[i]) {
+			while(num >= arabic_arr[i]) {
+				num -= arabic_arr[i];	
+				tamp += roman_arr[i];
+			}	
+		}
+	}
+	return tamp;
 }
 
 // Drive code
@@ -11,3 +28,6 @@ console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
+
+
+
